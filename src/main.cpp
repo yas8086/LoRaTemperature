d36f2564ac55fe6e1295f85a6f0865c57c4efe06
UbuntuHex <yas8086@qq.com>
2026-06-29 12:42:41 +0800

    feat: 集成 main.cpp 完成应用

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <QApplication>
+#include "MainWindow.h"
+
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    app.setOrganizationName("LoRaTemperature");
+    app.setApplicationName("LoRaTemperature");
+
+    MainWindow w;
+    w.show();
     return app.exec();
 }
